b353d4bb666dabdf4497a43771b1b91fda7b9ae1
JPeer264 <jan.oster94@gmail.com>
2017-10-21 18:24:51 +0200

    Docs: add example usage

---
+++ example/README.md
@@ -0,0 +1,19 @@
+# Pipefi example
+
+## Usage
+
+> Make sure you have installed all dependencies via `npm i`
+
+```sh
+# 1. go into the example folder
+$ cd example
+
+# 2. run the last pipe as consumer
+$ node consumer.js
+
+# 3. start pipefi
+$ node index.js
+
+# 4. send any message
+$ node publisher.js Filter this message
+```
--- example/publisher.js
+++ example/publisher.js
@@ -5,12 +5,14 @@ const config = require('./config');
 (async () => {
   try {
     const queue = 'lowercase';
+    const message = process.argv.slice(2, process.argv.length).join(' ') || 'THIS is A TEST STrING    ';
+
     const AMQP_URL = `amqp://${config.connection.user}:${config.connection.password}@${config.connection.host}:${config.connection.port}`;
     const connection = await amqp.connect(AMQP_URL);
     const channel = await connection.createChannel();
 
     await channel.assertQueue(queue);
-    await channel.sendToQueue(queue, new Buffer('THIS is A TEST STrING    '));
+    await channel.sendToQueue(queue, new Buffer(message));
   } catch (e) {
     console.error(e);
   }
